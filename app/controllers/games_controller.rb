5fb69010c63d48f5b5c0ce67b4327fd30ad19488
Pierre David <pierreaymeric.david@gmail.com>
2020-11-09 21:19:11 +0100

    correction

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,7 +10,8 @@ class GamesController < ApplicationController
   def score
     @proposition = params[:word].upcase.split("").reject{|x| x==" "}
     @random_letters = params[:letters].split()
-    @build_test = @proposition.all? {|x| @random_letters.include?(x)}
+
+    @build_test = @proposition.all? { |letter| @proposition.count(letter) <= @random_letters.count(letter) }
 
     url = "https://wagon-dictionary.herokuapp.com/#{@proposition.join("")}"
     @dico_test = JSON.parse(open(url).read)["found"]
